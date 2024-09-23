a7627ba6654a8832f21863c41045c7cb756294d6
aoi3142 <aperture@outlook.sg>
2024-09-23 13:50:09 +0800

    Improve caching for vocab permutation

--- Watermark/Permute.py
+++ Watermark/Permute.py
@@ -1,19 +1,43 @@
 import numpy as np
+import psutil
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            # Move the accessed item to the end of the OrderedDict to mark it as recently used.
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            # Update the value and move it to the end.
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            # Remove the first key-value pair which is the least recently used.
+            self.cache.popitem(last=False)
 
 class Permute:
     def __init__(self, N):
         self.N = N
-        self.permutations = {}
+        cache_size = int(psutil.virtual_memory().total / 10 / N / 8) # 10% of memory: vocab_size * 8 bytes per long
+        self.permutations = LRUCache(cache_size)
 
     def get_permutation(self, prev_tok, id = None, cache = False):
         assert not (id is None), "id must be provided to permute"
         key = (id, *prev_tok)
         if cache:
-            if key in self.permutations:
-                permutation = self.permutations[permutation]
-            else:
+            permutation = self.permutations.get(key)
+            if permutation == -1:
                 permutation = np.random.RandomState(key).permutation(self.N)
-                self.permutations[permutation] = permutation
+                self.permutations.put(key, permutation)
         else:
             permutation = np.random.RandomState(key).permutation(self.N)
         return permutation
--- Watermark/WatermarkerBase.py
+++ Watermark/WatermarkerBase.py
@@ -31,7 +31,7 @@ class PerturbationProcessor(LogitsProcessor):
     def set_phi(self, phi):
         self.phi = phi
 
-    def get_permutation(self, prev_tok, id = None, cache = False):
+    def get_permutation(self, prev_tok, id = None, cache = True):
         if id is None:
             id = self.id
         return self.permute.get_permutation(prev_tok, id, cache)
@@ -58,10 +58,12 @@ class Watermarker:
 
         self.model = model
         self.tokenizer = tokenizer
+        self.id = id
+        self.k_p = k_p
         self.n_gram = n_gram
         self.kappa = kappa
 
-        self.N = len(self.tokenizer)
+        self.N = self.tokenizer.vocab_size
         self.logits_processor = PerturbationProcessor(N = self.N, id = id)
 
         self.watermarking_fn = watermarkingFnClass(id = id, k_p = k_p, N = self.N, kappa = kappa)
@@ -70,7 +72,7 @@ class Watermarker:
 
         self.logits_processor.set_phi(self.phi)
 
-    def generate(self, prompt = None, n_gram = None):
+    def generate(self, prompt = None, n_gram = None, max_new_tokens = 1000, return_text=True, return_tokens=False, return_scores=False, do_sample=True, **kwargs):
         if n_gram is None:
             n_gram = self.n_gram
         tokd_input = self.tokenizer(prompt, return_tensors="pt", add_special_tokens=False).to(self.model.device)
@@ -78,14 +80,38 @@ class Watermarker:
             self.logits_processor.reset(n_gram)
             output = self.model.generate(
                 **tokd_input, 
-                max_new_tokens=1000, 
-                do_sample=True, 
-                logits_processor=([self.logits_processor] if self.kappa > 0 else None)
+                max_new_tokens=max_new_tokens, 
+                do_sample=do_sample,
+                logits_processor=([self.logits_processor] if self.kappa > 0 else None),
+                pad_token_id=self.tokenizer.eos_token_id,
+                **kwargs
                 )
-        output = output[:,tokd_input["input_ids"].shape[-1]:]
-        decoded_output = self.tokenizer.batch_decode(output, skip_special_tokens=True)[0].strip()
+        output = output[:,tokd_input["input_ids"].shape[-1]:].cpu()
 
-        return decoded_output
+        return_dict = {}
+
+        if return_scores:
+            cumulative_token_count = self.get_cumulative_token_count(output, id = self.logits_processor.id)
+            res = self.q(cumulative_token_count)
+            q_score = res[:,self.k_p-1]
+            return_dict["q_score"] = q_score
+
+        if return_tokens:
+            return_dict["tokens"] = output
+
+        if return_text:
+            decoded_output = self.tokenizer.batch_decode(output, skip_special_tokens=True)
+            decoded_output = [i.strip() for i in decoded_output]
+            return_dict["text"] = decoded_output
+
+        if len(output) == 1:
+            for k, v in return_dict.items():
+                return_dict[k] = v[0]
+
+        if return_text and len(return_dict) == 0:
+            return decoded_output
+
+        return return_dict
 
     def get_cumulative_token_count(self, tokens, n_gram = 2, id = None):
         if tokens.ndim == 1:
@@ -96,6 +122,8 @@ class Watermarker:
             for j in range(window, tokens.shape[1]):
                 prev_token = tokens[i, j-window:j]
                 t = tokens[i,j]
+                if t >= self.tokenizer.vocab_size or t == self.tokenizer.eos_token_id:  # EOS token
+                    break
                 indices = self.logits_processor.get_permutation(prev_token, id = id)
                 x = indices[t]
                 cumulative_token_count[i, x] += 1
